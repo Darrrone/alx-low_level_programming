78947f1ec4a9c82906699ec47da034e4e8f49c33
Darrrone <daraone2@gmail.com>
2022-06-16 16:08:00 -0700

    pointers arrays strings 2

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -34,21 +34,21 @@ add = op / 10;
 else
 add = 0;
 if (op > 0)
-	*(r + bg) = (op % 10) + 48;
-      else
-	*(r + bg) = '0';
-      if (c1 > 0)
-	c1--, dr1 = *(n1 + c1) - 48;
-      else
-	dr1 = 0;
-      if (c2 > 0)
-	c2--, dr2 = *(n2 + c2) - 48;
-      else
-	dr2 = 0;
-      bg--, size_r--;
-    }
-  if (*(r) == '0')
-    return (r + 1);
-  else
-    return (r);
+*(r + bg) = (op % 10) + 48;
+else
+*(r + bg) = '0';
+if (c1 > 0)
+c1--, dr1 = *(n1 + c1) - 48;
+else
+dr1 = 0;
+if (c2 > 0)
+c2--, dr2 = *(n2 + c2) - 48;
+else
+dr2 = 0;
+bg--, size_r--;
+}
+if (*(r) == '0')
+return (r + 1);
+else
+return (r);
 }
