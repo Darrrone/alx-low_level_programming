3f5cae7c2010ee28f56c82ba8a29054ee01fd4b0
Darrrone <daraone2@gmail.com>
2022-06-20 16:15:20 -0700

    pointers arrays strings 3

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,14 +6,18 @@
  *@n: bytes of the memory
  *Return: dest
  */
+
 char *_memset(char *s, char b, unsigned int n)
+
 {
 unsigned int i;
 
-for (i = 0; n > 0 ; i++, n--)
+/*Delacring FOR*/
+for (i = 0; i < n; i++)
 {
-s[i] = b;
-}
+*(s + i) = b; /*add 1 position s*/
+
+} /*END FOR*/
 
 return (s);
 }
