ab094fdf0d23baf07bec1f3f41ab96f46ef5bf57
Darrrone <daraone2@gmail.com>
2022-07-06 16:05:36 -0700

    function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -13,7 +14,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
 
-if (array && action)
+if (!array || !action)
+return;
+
 for (i = 0; i < size; i++)
 action(array[i]);
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,12 +17,11 @@ int i;
 
 if (array && cmp)
 {
-if (size <= 0)
-return (-1);
-
 for (i = 0; i < size; i++)
-if (cmp(array[i]))
+{
+if (cmp(array[i]) != 0)
 return (i);
+}
 }
 
 return (-1);
